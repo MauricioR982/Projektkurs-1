f592e00caab669312cc92c4accd845ba458a4293
Sergeant752 <sabbirkc98@gmail.com>
2024-04-20 10:15:47 +0200

    code for player position

--- network.h
+++ network.h
@@ -10,6 +10,7 @@
 typedef struct {
     IPaddress address;
     bool connected;
+    int x, y;          // Player position
 } ClientInfo;
 
 int network_init(char* host, Uint16 port, bool isServer);
@@ -17,6 +18,7 @@ void network_check_activity(Uint32 timeout);
 void network_handle_server();
 void network_handle_client();
 int find_or_add_client(IPaddress newClientAddr);
+void update_player_position(int playerIndex, int x, int y);
 void network_cleanup();
 
 #endif // NETWORK_H
